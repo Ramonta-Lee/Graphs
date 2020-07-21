82a277b987d758d24799a3e6bdab3b78fef1a35e
unknown <viola4lfe@gmail.com>
2020-07-21 13:00:00 -0400

    movement

--- projects/adventure/room.py
+++ projects/adventure/room.py
@@ -1,5 +1,6 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+# room information
 class Room:
     def __init__(self, name, description, id=0, x=None, y=None):
         self.id = id
@@ -11,6 +12,7 @@ class Room:
         self.w_to = None
         self.x = x
         self.y = y
+
     def __str__(self):
         return f"\n-------------------\n\n{self.name}\n\n   {self.description}\n\n{self.get_exits_string()}\n"
     def print_room_description(self, player):
